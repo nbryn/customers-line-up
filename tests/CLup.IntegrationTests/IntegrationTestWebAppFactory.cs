ca8750f954bf6e60bf9a38be9f6e5c4ba405c602
Niklas Brynfeldt <niklasbg@live.dk>
2024-01-09 19:34:53 +0100

    Working on integration tests setup

---
+++ tests/CLup.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,41 @@
+﻿using CLup.API;
+using CLup.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MsSql;
+
+namespace tests.CLup.IntegrationTests;
+
+public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2019-latest")
+        .WithPassword("Secret_paSS123")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptor =
+                services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<CLupDbContext>));
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<CLupDbContext>(options =>
+            {
+                options.UseSqlServer(_dbContainer.GetConnectionString());
+            });
+        });
+    }
+
+    public Task InitializeAsync() => _dbContainer.StartAsync();
+
+    public Task DisposeAsync() => _dbContainer.StopAsync();
+}
